09a283aa68012b7fd13ac9665ae739d179c9997c
DanyloSus <danylo.sushko19@gmail.com>
2023-01-08 17:57:04 +0200

    Random array

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
-import React from "react";
-import Die from "./components/Die";
+import React from "react"
+import Die from "./components/Die"
 
 export default function App() {
-  return(
-    <main>
-      <Die value={1}/>
-      <Die value={1}/>
-      <Die value={1}/>
-      <Die value={1}/>
-      <Die value={1}/>
-      <Die value={1}/>
-      <Die value={1}/>
-      <Die value={1}/>
-      <Die value={1}/>
-      <Die value={1}/>
-    </main>
-  );
+    function allNewDice() {
+        const newDice = []
+        for (let i = 0; i < 10; i++) {
+            newDice.push(Math.ceil(Math.random() * 6))
+        }
+        return newDice
+    }
+    console.log(allNewDice())
+    
+    return (
+        <main>
+            <div className="dice-container">
+                <Die value="1" />
+                <Die value="2" />
+                <Die value="3" />
+                <Die value="4" />
+                <Die value="5" />
+                <Die value="6" />
+                <Die value="1" />
+                <Die value="1" />
+                <Die value="1" />
+                <Die value="1" />
+            </div>
+        </main>
+    )
 }
