9736774853dc1f73146cc4c313b108161c9096be
DanyloSus <danylo.sushko19@gmail.com>
2023-01-08 20:23:15 +0200

    My version of holding dice

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
 import React from "react"
 import Die from "./components/Die"
-
+import {nanoid} from "nanoid"
 
 export default function App() {
+/**
+ * Challenge: Add conditional styling to the Die component
+ * so that if it's held (isHeld === true), its background color
+ * changes to a light green (#59E391)
+ * 
+ * Remember: currently the Die component has no way of knowing
+ * if it's "held" or not.
+ */
+
     const [dice, setDice] = React.useState(allNewDice())
     
+    function handleChange(id) {
+      setDice(oldDice => {
+        return oldDice.map(oldDie => {
+          return (
+            oldDie.id === id ? {
+              ...oldDie,
+              isHeld: !oldDie.isHeld
+            }:
+              oldDie
+          )
+      })})
+    }
+
     function allNewDice() {
         const newDice = []
         for (let i = 0; i < 10; i++) {
-            newDice.push({ 
-              value: Math.ceil(Math.random() * 6),
-              isHeld: false
-          })
+            newDice.push({
+                value: Math.ceil(Math.random() * 6), 
+                isHeld: false,
+                id: nanoid()
+            })
         }
         return newDice
     }
     
     function rollDice() {
-        setDice(allNewDice())
+        return setDice(oldDice => oldDice.map(oldDie => {
+          return (
+            oldDie.isHeld === false ?
+              {...oldDie, value: Math.ceil(Math.random() * 6)} :
+              oldDie
+          )
+        }))
     }
     
-    const diceElements = dice.map(die => <Die value={die.value} />)
+    const diceElements = dice.map(die => (
+        <Die 
+          key={die.id} 
+          value={die.value} 
+          handleChange={() => handleChange(die.id)}
+          isHeld={die.isHeld}
+        />
+    ))
     
     return (
         <main>
--- src/components/Die.js
+++ src/components/Die.js
@@ -2,7 +2,10 @@ import React from "react"
 
 export default function Die(props) {
     return (
-        <div className="die-face">
+        <div 
+            onClick={props.handleChange}  
+            className={props.isHeld ? "die-face on" : "die-face"}
+        >
             <h2 className="die-num">{props.value}</h2>
         </div>
     )
